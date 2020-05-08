3bff4c03efeb07721c72b6e470ffb5d878c021e5
min103jun <min103jun@naver.com>
2020-05-08 18:01:16 +0900

    Update 다트게임.cpp

--- 프로그래머스/다트게임.cpp
+++ 프로그래머스/다트게임.cpp
@@ -1,71 +1,44 @@
 #include <string>
-#include <iostream>
-#include <vector>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
-
 int solution(string dartResult) {
-	int i, j, answer = 0, index = 0;
-	vector<string> resultsplit;
-	vector<int> answerarr;
-	string opt = "", mult_s;
-	double num, mult_d;
+    int i, index, answer = 0, cnt = 0;
+    int score[3] = { 0, 0, 0 };
 
-	for (i = 1; i < dartResult.size(); i++) {
-		if (48 <= dartResult[i] && dartResult[i] <= 57) {
-			if (dartResult[i] == 48) {
-				if (dartResult[i - 1] != 49) {
-					resultsplit.push_back(dartResult.substr(index, i - index));
-					index = i;
-				}
-			}
-			else {
-				resultsplit.push_back(dartResult.substr(index, i - index));
-				index = i;
-			}
-		}
-	}
-	resultsplit.push_back(dartResult.substr(index, i - index));
-	
-	for (i = 0; i < resultsplit.size(); i++) {
-		if (resultsplit[i][1] == 48) {
-			num = 10;
-			mult_s = resultsplit[i].substr(2, 1);
-			index = 3;
-		}
-		else {
-			num = stoi(resultsplit[i].substr(0, 1));
-			mult_s = resultsplit[i].substr(1, 1);
-			index = 2;
-		}
-		if (index != resultsplit[i].size())
-			opt = resultsplit[i].substr(index, 1);
-		
-		if (mult_s == "S")
-			mult_d = 1;
-		else if (mult_s == "D")
-			mult_d = 2;
-		else
-			mult_d = 3;
+    index = 0;
+    for (i = 0; i < 3; i++) {
+        if ('0' <= dartResult[index] && dartResult[index] <= '9') {
+            if (dartResult[index + 1] == '0') {
+                score[i] = 10;
+                index += 2;
+            }
+            else {
+                score[i] = dartResult[index] - '0';
+                ++index;
+            }
+        }
 
-		if (opt == "*") {
-			if (i == 0)
-				answerarr.push_back((int)pow(double(num), mult_d) * 2);
-			else {
-				answerarr[i - 1] *= 2;
-				answerarr.push_back((int)pow(double(num), mult_d) * 2);
-			}
-		}
-		else if (opt == "#")
-			answerarr.push_back((int)pow(double(num), mult_d) * (-1));
-		else if (opt == "")
-			answerarr.push_back((int)pow(double(num), mult_d));
-		opt = "";
-	}
-		
-	for (i = 0; i < 3; i++)
-		answer += answerarr[i];
-	return answer;
+        if (dartResult[index] == 'S')
+            score[i] = pow(score[i], 1);
+        else if (dartResult[index] == 'D')
+            score[i] = pow(score[i], 2);
+        else
+            score[i] = pow(score[i], 3);
+        ++index;
+
+        if (dartResult[index] == '*') {
+            score[i] *= 2;
+            if (i != 0)
+                score[i - 1] *= 2;
+            ++index;
+        }
+        else if (dartResult[index] == '#') {
+            score[i] *= (-1);
+            ++index;
+        }
+    }
+
+    return score[0] + score[1] + score[2];
 }
