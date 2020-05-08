37c307239cbbef5760c33cbca3c2bc3ebded17a4
min103jun <min103jun@naver.com>
2020-05-08 17:37:13 +0900

    Update 뉴스클러스트링.cpp

--- 프로그래머스/뉴스클러스트링.cpp
+++ 프로그래머스/뉴스클러스트링.cpp
@@ -1,56 +1,44 @@
 #include <string>
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <set>
+#include <vector>
 
 using namespace std;
 
 int solution(string str1, string str2) {
-    int i, j, answer;
-    float sum = 0, mul = 0;
-	float temp;
-	vector<string> str1_arr, str2_arr;
+//set_intersection을 하려면 1. 컨테이너가 벡터일 것, 2. 두 벡터가 정렬되어 있을 것, 3. 담을 벡터의 사이즈가 정해져 있어야 한다.
+    int i, j, answer = 0, sz1, sz2, intersection, sum;
+    vector<string> S1, S2, S3; //1.조건 컨테이너를 벡터로 잡음
+    string tmp;
 
-	//모두 소문자로
-	for(i = 0; i < str1.size(); i++) 
-        str1[i] = tolower(str1[i]);
-    for(i = 0; i < str2.size(); i++)
-        str2[i] = tolower(str2[i]);
-    
-	//두글자씩 끊어서 배열에 저장
-	for (i = 0; i < str1.size() - 1; i++) {
-		if (str1[i] < 97 || str1[i] > 122)
-			continue;
-		if (str1[i + 1] < 97 || str1[i + 1] > 122)
-			continue;
-		str1_arr.push_back(str1.substr(i, 2));
-	}
-	for (i = 0; i < str2.size() - 1; i++) {
-		if (str2[i] < 97 || str2[i] > 122)
-			continue;
-		if (str2[i + 1] < 97 || str2[i + 1] > 122)
-			continue;
-		str2_arr.push_back(str2.substr(i, 2));
-	}
+    sz1 = str1.size() - 1;
+    for (i = 0; i < sz1; i++) {
+        tmp = str1.substr(i, 2);
+        transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
+        if ('a' <= tmp[0] && tmp[0] <= 'z' && 'a' <= tmp[1] && tmp[1] <= 'z') {
+            S1.push_back(tmp);
+        }
+    }
+    sz2 = str2.size() - 1;
+    for (i = 0; i < sz2; i++) {
+        tmp = str2.substr(i, 2);
+        transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
+        if ('a' <= tmp[0] && tmp[0] <= 'z' && 'a' <= tmp[1] && tmp[1] <= 'z') {
+            S2.push_back(tmp);
+        }
+    }
 
-	//교집합 갯수 구하기. 합집합은 두 집합의 갯수에서 교집합 수만큼 빼주면 된다.
-	mul = str1_arr.size() + str2_arr.size();
-	for (i = 0; i < str1_arr.size(); i++) {
-		for (j =0 ; j < str2_arr.size(); j++) {
-			if (str1_arr[i] == str2_arr[j]) {
-				sum++;
-				str2_arr.erase(str2_arr.begin() + j);
-				break;
-			}
-		}
-	}
-
-	//출력
-    mul -= sum;
-	if (mul == 0) 
-		answer = 65536;
-	else 
-		answer = sum / mul * 65536;
-
-	return answer;
+    sz1 = S1.size();
+    sz2 = S2.size();
+    sort(S1.begin(), S1.end()); 
+    sort(S2.begin(), S2.end()); //조건2. 두개의 벡터를 정렬해야함
+    S3.resize(S1.size() + S2.size());//조건3. 담을 벡터의 사이즈를 정해야 함
+    vector<string>::iterator it = set_intersection(S1.begin(), S1.end(), S2.begin(), S2.end(), S3.begin());
+    S3.erase(it, S3.end()); //미리 정해둔 사이즈. set_intersection의 반환값은 dset 벡터의 마지막 위치이다.
+    intersection = S3.size();
+    sum = sz1 + sz2 - intersection;
+    if (sum == 0)
+        return 65536;
+    else
+        return intersection * 65536 / sum;
 }
