37c434a68b0be02d31cc6b00d419302a42609d41
min103jun <min103jun@naver.com>
2020-05-31 21:33:54 +0900

    마름모를 그리는 두가지 방법

--- swexpertacademy/[2105]디저트카페.cpp
+++ swexpertacademy/[2105]디저트카페.cpp
@@ -1,3 +1,4 @@
+//첫번째 풀이 - dfs로 그려가며 바로바로 리턴하는 
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -73,4 +74,108 @@ int main()
 			cout << "#" << i << " " << answer << "\n";
 	}
 	return 0;
+}
+
+//두번째 방식 - 일단 d1, d2를 정해두고 그려보며 판단하는 방식
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int answer, N;
+int map[20][20];
+int dir[4][2] = { {1, 1}, {1, -1}, {-1, -1}, {-1, 1} };
+bool visit[101];
+
+bool circle(int x, int y, int d1, int d2)
+{
+	int tx = x;
+	int ty = y;
+
+	for (int i = 0; i < 3; i++) {
+		if (i == 0 || i == 2) {
+			tx += (dir[i][0] * d1);
+			ty += (dir[i][1] * d1);
+		}
+		else {
+			tx += (dir[i][0] * d2);
+			ty += (dir[i][1] * d2);
+		}
+		if (tx < 0 || ty < 0 || tx >= N || ty >= N)
+			return false;
+	}
+
+	return true;
+}
+
+void check(int x, int y, int d1, int d2)
+{
+	int i, j, k, d, cnt = 0;
+	int tx, ty, nx, ny;
+
+	memset(visit, false, sizeof(bool) * 101);
+	tx = x;
+	ty = y;
+	for (i = 0; i < 4; i++) {
+		if (i == 0 || i == 2)
+			d = d1;
+		else
+			d = d2;
+		for (j = 0; j < d; j++) {
+			tx += dir[i][0];
+			ty += dir[i][1];
+			if (visit[map[tx][ty]] == false) {
+				visit[map[tx][ty]] = true;
+				++cnt;
+			}
+			else
+				return;
+		}
+	}
+	answer = max(cnt, answer);
+}
+
+void solution()
+{
+	int i, j, d1, d2;
+
+	cin >> N;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	for (d1 = 1; d1 < N; d1++) {
+		for (d2 = 1; d2 < N; d2++) {
+			for (i =0; i < N - 2; i++) {
+				for (j = 1; j < N - 1; j++) {
+					if (circle(i, j, d1, d2)) {
+						check(i, j, d1, d2);
+					}
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int i, T;
+
+	cin >> T;
+	for (i = 1; i <= T; i++) {
+		answer = 0;
+		solution();
+		if(answer == 0)
+			cout << "#" << i << " -1" << endl;
+		else
+			cout << "#" << i << " " << answer << endl;
+	}
 }
